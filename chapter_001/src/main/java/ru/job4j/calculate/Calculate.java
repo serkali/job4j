4b177fb80d706cb65751d4f426069910f5f3e8c4
Serkali Bakishev <serkali88@gmail.com>
2019-02-28 15:13:08 +0400

    1.3. JavaDoc. Code convention[#112508]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,14 @@
 package ru.job4j.calculate;
+/**
+*Class Класс для вывода сообщения на консоль
+*@author sbakishev
+*@version 1
+/*
 public class Calculate {    
+/**
+*Method main.
+*@param args - args.
+*/
     public static void main(String[] args )     {
         System.out.println("Hello World");    
     }    
