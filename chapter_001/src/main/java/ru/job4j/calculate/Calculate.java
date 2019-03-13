917d954d6a727aa8eff2cfe520d7b2163b9d9da0
Serkali Bakishev <serkali88@gmail.com>
2019-03-13 21:58:42 +0400

    1.5. Checkstyle[#112510]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -11,7 +11,7 @@ public class Calculate {
 *Method main.
 *@param args - args.
 */
-    public static void main(String[] args){
+    public static void main(String[] args)  {
       System.out.println("Hello World");    
     } 
 /**
