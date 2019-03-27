a70763c8008fabd6da00b91b117372a1d58801a4
Serkali Bakishev <serkali88@gmail.com>
2019-03-27 18:40:51 +0400

    3.1. Элементарный калькулятор.[#112499]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculator.java
@@ -1,16 +1,50 @@
 package ru.job4j.сalculator;
+
+/**
+ * Элементарный калькулятор
+ * @author SBakishev
+ * version 1
+ */
 public class Calculator {
+    /**
+     * Method сложение
+     * @param first
+     * @param second
+     * @return
+     */
     public double add (double first, double second ) {
         return first + second;
     }
+
+    /**
+     * Method вычитание
+     * @param first
+     * @param second
+     * @return
+     */
     public double subtract (double first, double second ) {
         return first - second;
 
     }
+
+    /**
+     * Method деление
+     * @param first
+     * @param second
+     * @return
+     */
     public double div (double first, double second ) {
         return first / second;
 
+
     }
+
+    /**
+     * Мethof умножения
+     * @param first
+     * @param second
+     * @return
+     */
     public double multiple (double first, double second) {
         return first * second;
     }
