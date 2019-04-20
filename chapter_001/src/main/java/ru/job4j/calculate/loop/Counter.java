0dee25d4ada3b86cf1f4b58f99fc9fca575e042f
serkali88 <serkali88@gmail.com>
2019-04-21 00:57:52 +0400

    5.1. Подсчет суммы чётных чисел в диапазоне[#112479]

--- chapter_001/src/main/java/ru/job4j/calculate/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/calculate/loop/Counter.java
@@ -13,7 +13,7 @@ public class Counter {
      * @param finish
      * @return sum
      */
-    public int add(int start, int finish {
+    public int add(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) { //цикл увеличивает переменную на единицу в диапозоне от start до finish
             if ((i % 2) == 0) { // проверяет четное ли число
--- chapter_001/src/test/java/ru/job4j/calculate/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/loop/CounterTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertThat;
 public class CounterTest {
     @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty(){
-        Counter counter = new Counter(;
+        Counter counter = new Counter();
         int result = counter.add(2, 11);
         assertThat(result, is(30));
     }
