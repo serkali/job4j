1456bb53ebba58299f88517747972aa82e49f894
serkali88 <serkali88@gmail.com>
2019-07-02 16:07:42 +0400

    5.5. Рефакторинг кода.[#112477]

--- chapter_001/src/main/java/ru/job4j/calculate/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/calculate/array/Check.java
@@ -3,11 +3,12 @@ package ru.job4j.calculate.array;
 public class Check {
     public boolean mono(boolean[] data) {
         boolean result = false;
-        for (int i = 0; i < data.length-1; i++){
-            if(data[i] != data[i+1]){
-                return false;
+        for (int i = 1; i < data.length; i++) {
+            if (data[0] != data[i]) {
             }
         }
-        return true;
+        return result;
     }
 }
+
+
