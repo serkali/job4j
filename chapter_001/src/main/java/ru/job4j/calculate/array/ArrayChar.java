4eccff48f06645ac50a6ce6019504ab56c6eaf09
serkali88 <serkali88@gmail.com>
2019-07-30 21:10:39 +0400

    6.4. Слово начинается с ...[#112488]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/array/ArrayChar.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculate.array;
+
+public class ArrayChar {
+    /**
+     * Проверяет. что слово начинается с префикса.
+     *
+     * @param prefix префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean Startswith(String word, String prefix) {
+        boolean result = true;
+        char[] pref = prefix.toCharArray();
+        char[] wrd = word.toCharArray();
+        for (int i = 0; i < pref.length; i++) {
+            if (pref[i] != wrd[i]) {
+                result = false;
+
+            }
+        }
+        return result;
+
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/array/ArrayCharTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculate.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar();
+        boolean result = word.Startswith("Hello", "He");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar();
+        boolean result = word.Startswith("Hello", "Hi");
+        assertThat(result, is(false));
+    }
+}
