273c61afc9090b7a49da480212bf86ab0841f259
serkali88 <serkali88@gmail.com>
2021-06-09 23:04:40 +0400

    6.5.3. Сортировка выборкой. [#156320 #166035]

--- chapter_001/src/main/java/ru/job4j/calculate/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/calculate/array/SortSelected.java
@@ -5,12 +5,18 @@ public class SortSelected {
         for (int i = 0; i < data.length - 1; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1); //минимальный элемент
             int index = FindLoop.indexOf(data, min, i, data.length - 1); // индекс минимального элемента
-            int tmp = data[i];
-            data[i] = data[index];
-            data[index] = tmp;
+            swap(data, i, index);
         }
+
         //swap(...)
         return data;
+    }
+
+    public static int[] swap(int[] array, int source, int dest) {
+        int tmp = array[source];
+        array[source] = array[dest];
+        array[dest] = tmp;
+        return array;
     }
 
 }
--- chapter_001/src/test/java/ru/job4j/calculate/array/SortSelectedTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/array/SortSelectedTest.java
@@ -6,9 +6,26 @@ import org.junit.Test;
 public class SortSelectedTest {
     @Test
     public void whenSort() {
-        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] data = new int[]{3, 4, 1, 2, 5};
         int[] result = SortSelected.sort(data);
-        int[] expected = new int[] {1, 2, 3, 4, 5};
+        int[] expected = new int[]{1, 2, 3, 4, 5};
         Assert.assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void whenSortZero() {
+        int[] data = new int[]{5, 0, 2, 4, 3, 1};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[]{0, 1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortNegative() {
+        int[] data = new int[]{0, 2, -1, 1};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[]{-1, 0, 1, 2};
+        Assert.assertArrayEquals(expected, result);
+    }
+
 }
