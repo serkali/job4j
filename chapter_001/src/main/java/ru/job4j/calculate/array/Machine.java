9dc27430fd575c2aea21b8c93226f6d3674fce24
serkali88 <serkali88@gmail.com>
2021-06-14 21:23:30 +0400

    6.5.2. Поиск минимума в диапазоне. [#179804]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/array/Machine.java
@@ -0,0 +1,29 @@
+package ru.job4j.calculate.array;
+
+import java.util.Arrays;
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};  //массив монет в автомате
+        int[] rsl = new int[100];  //сдача в виде массива монет
+        //размер массива rsl , которого он достигнет после того, как автомат отдаст всю сдачу
+        int size = 0;
+        //money - купюра из кармана;
+        //price - цена покупки
+        //определяем размер сдачи
+        int change = money - price;
+        for (int i = 0; i < coins.length; i++) {
+            //условие отбора монет автоматом
+            while (change - coins[i] >= 0) {
+                rsl[size] = coins[i];
+                change -= coins[i];
+                size++;
+
+            }
+
+        }
+        return Arrays.copyOf(rsl, size);
+
+    }
+
+}
